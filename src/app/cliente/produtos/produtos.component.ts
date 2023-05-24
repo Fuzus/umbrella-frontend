61bcf561d7db9e122892df1a5e4a829a22c3606c
Fuzus <matheusdamata2000@gmail.com>
2023-05-23 23:07:40 -0300

    adcionando validação de se ha um usuario logado ao iniciar a home

--- src/app/cliente/produtos/produtos.component.ts
+++ src/app/cliente/produtos/produtos.component.ts
@@ -1,27 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/_models/product';
 import { ProductService } from 'src/app/_services/product.service';
+import { AuthGuard } from 'src/app/auth.guard';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
   styleUrls: ['./produtos.component.css']
 })
-export class ProdutosComponent implements OnInit{
+export class ProdutosComponent implements OnInit {
 
   products: Product[] = [];
 
   constructor(
-    private productService: ProductService
-  ){}
+    private productService: ProductService,
+    private authGuard : AuthGuard
+  ) { }
 
-    ngOnInit(): void {
-        this.productService.getAllCliente().subscribe(res => {
-          this.products = res;
-          for (let product of this.products){
-            if (product.images)
-              product.cover = product.images.find(x => x.type == 1);
-          }
-        })
-    }
+  ngOnInit(): void {
+    this.authGuard.isLoggedIn()
+    this.productService.getAllCliente().subscribe(res => {
+      this.products = res;
+      for (let product of this.products) {
+        if (product.images)
+          product.cover = product.images.find(x => x.type == 1);
+      }
+    })
+  }
 }
